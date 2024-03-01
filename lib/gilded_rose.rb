dadfadf015eee3d4e4a7cb5c9a29613a09ba8479
Naitik Kaythwal <naitikkaythwal@gmail.com>
2024-02-29 20:24:45 -0600

    refactor -2

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,15 +8,25 @@ class GildedRose
   end
 
   def normal_tick
-    days_remaining -=1
-    return if quantity == 0
-    @quantity-=1
-    @quantity -=1 if @days_remaining <=0
+    @days_remaining -=1
+    return if @quality == 0
+    @quality-=1
+    @quality -=1 if @days_remaining <=0
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining <=0 && @quality < 50
   end
 
   def tick
-    if name == 'normal'
+    case name
+    when 'normal'
       return normal_tick
+    when 'Aged Brie'
+      return brie_tick
     end
 
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
