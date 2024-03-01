7f16689f82ae8729d5b4fb755d1b59d395c38f5a
Naitik Kaythwal <naitikkaythwal@gmail.com>
2024-02-29 20:43:58 -0600

    refactor -5

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,10 +8,11 @@ class GildedRose
   end
 
   def normal_tick
-    @days_remaining -=1
+    @days_remaining -= 1
     return if @quality == 0
-    @quality-=1
-    @quality -=1 if @days_remaining <=0
+
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
   end
 
   def brie_tick
@@ -38,7 +39,7 @@ class GildedRose
 
   def tick
     case name
-    when 'normal'
+    when 'Normal Item'
       return normal_tick
     when 'Aged Brie'
       return brie_tick
@@ -46,50 +47,6 @@ class GildedRose
       return sulfuras_tick
     when 'Backstage passes to a TAFKAL80ETC concert'
       return backstage_tick
-    end
-
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
     end
   end
 end
